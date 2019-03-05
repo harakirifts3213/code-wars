8332c144b16b61f72f5ff07301053df885a6ecdc
Michael Harmon <harakirifts3213@gmail.com>
2019-03-04 18:53:03 -0700

    moved to CodeWars

---
+++ Javascript/index.js
@@ -0,0 +1,27 @@
+const me = {
+    name: 'Michael',
+    age: 20,
+    height: `5'8`,
+    weight: 200,
+    birthday: `5/12/1998`,
+    email: 'obey3213@outlook.com',
+    address:{
+        street: `1914 Lakewood Drive`,
+        city: 'Holladay',
+        state: 'Utah'
+        ZipCode: `84117`,
+    },
+myFavoriteFoods: [`Pizza`, `Steak`, `Tacos`, `Tika Masala`],
+myFamily: [
+    {
+        name: `Emily`,
+        age: 21,
+        height: `5'4"`,
+    },
+    {
+        name: `Isaac`,
+        age: 16,
+        height: `5'7"`,
+    },
+],
+};
