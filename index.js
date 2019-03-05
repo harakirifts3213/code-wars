bfa63fa6cf57f92366823f1269bc607b5f0d7eb1
Michael Harmon <harakirifts3213@gmail.com>
2019-03-04 18:42:44 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const me = {
+    name: 'Michael',
+    age: 20,
+    height: `5'8`,
+    weight: 200,
+    birthday: `5/12/1998`,
+    email: 'obey3213@outlook.com',
+    address:{
+        street: `1914 Lakewood Drive`,
+        city: 'Holladay',
+        state: 'Utah'
+        ZipCode: `84117`,
+    }
+};
